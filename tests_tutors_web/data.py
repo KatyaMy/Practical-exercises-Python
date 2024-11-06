58dec26948a169e49e227537db1bd243e669c093
KatyaMy <20FamGam20@gmail.com>
2024-11-05 20:07:02 -0500

    refactoring tests

--- tests_tutors_web/data.py
+++ tests_tutors_web/data.py
@@ -1,5 +1,5 @@
-username = "TestUser_1"
-password = "test5test78"
+user_name = "TestUser_1"
+c_password = "test5test78"
 
 incorrect_username = "TestUser"
 incorrect_password = "Passwordfortestuser123"
--- tests_tutors_web/page_login.py
+++ tests_tutors_web/page_login.py
@@ -1,6 +1,4 @@
-
 from tests_tutors_web.locators import Login
-from tests_tutors_web.data import *
 
 login_link = "http://195.133.27.184/login/"
 
@@ -9,7 +7,7 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def sign_in(self):
+    def sign_in(self, username, password):
         self.driver.get(login_link)
         title = self.driver.find_element(*Login.title)
         assert title.is_displayed(), 'Page Incorrect'
@@ -17,4 +15,8 @@ class LoginPage:
         self.driver.find_element(*Login.password).send_keys(password)
         self.driver.find_element(*Login.submit_button).click()
 
+    def get_login_error_text(self):
+        return self.driver.find_element(*Login.alert_required_field_login).text
 
+    def get_password_error_text(self):
+        return self.driver.find_element(*Login.alert_required_filed_password).text
